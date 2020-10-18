2fffb23b92ef2bcd83471396e70a0d02ca2f3bc2
aghniadm <aghniadewii@gmail.com>
2020-10-18 18:23:59 +0700

    PBO2-10119073-Latihan2-DeklarasiVariabel

---
+++ src/pbo2/pkg10119073/latihan2/deklarasivariabel/PBO210119073Latihan2DeklarasiVariabel.java
@@ -0,0 +1,36 @@
+package pbo2.pkg10119073.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author 
+ * NAMA     : Aghnia Dewi Mahiranie
+ * KELAS    : PBO2
+ * NIM      : 10119073
+ * Deskripsi Program : Program ini berisi program untuk mendeklarasi dan
+ * pengaksesan variabel
+ *
+ */
+public class PBO210119073Latihan2DeklarasiVariabel {
+
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai    = " + nilaiInt);
+        System.out.println("Isi variabel PHI      = " + PHI);
+        System.out.println("Isi variabel logika   = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+        
+    }
+    
+}
